3bb489a5a8912c3e21cebb9d21b29da33b6c553b
Christoph Junghans <junghans@votca.org>
2015-04-29 14:08:41 -0600

    benchmark.sh: add -v option

--- benchmark.sh
+++ benchmark.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 make
+[[ $1 = -v ]] && shift && verbose=yes
 cut="1.3"
 t0=$(mktemp mdljout.XXXX)
 for i in " " "-rvl $cut" "-rlc $cut" "-rvl $cut -rlc $cut"; do
   t1=$(mktemp mdljout.XXXX)
   echo ./mdlj $i "$@"
-  time ./mdlj $i "$@" | sed '/^#/d' > "$t1"
+  time ./mdlj $i "$@" > "$t1"
+  [[ $verbose ]] && cat "${t1}"
+  sed -i '/^#/d' "${t1}"
   echo "-------------------------"
   [[ -s $t0 ]] || cp "$t1" "$t0"
   cmp "$t0" "$t1" || echo "Results differ!!!"
