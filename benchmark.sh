632b9e7671cb5585251c8f4128429eff26a12153
Christoph Junghans <junghans@votca.org>
2018-01-01 13:20:46 -0700

    benchmark.sh: fix exit code and more verbose

--- benchmark.sh
+++ benchmark.sh
@@ -4,15 +4,21 @@ make
 [[ $1 = -v ]] && shift && verbose=yes
 cut="1.3"
 t0=$(mktemp mdljout.XXXX)
-for i in " " "-rvl $cut" "-rlc $cut" "-rvl $cut -rlc $cut"; do
+for i in "" "-rvl $cut" "-rlc $cut" "-rvl $cut -rlc $cut"; do
   t1=$(mktemp mdljout.XXXX)
+  [[ -z ${i0} ]] && i0="$i"
   echo ./mdlj $i "$@"
   time ./mdlj $i "$@" > "$t1"
   [[ $verbose ]] && cat "${t1}"
   sed -i '/^#/d' "${t1}"
   echo "-------------------------"
-  [[ -s $t0 ]] || cp "$t1" "$t0"
-  cmp "$t0" "$t1" || echo "Results differ!!!"
+  [[ ! -s $t0 ]] && cp "$t1" "$t0" && gold=$i
+  if cmp "$t0" "$t1"; then
+    [[ $i != $gold ]] && echo "Result of '$i' match with gold (${gold:-no args})"
+  else
+    echo "Result of '$i' differ from gold (${gold:-no args})" >&2
+    exit 1
+  fi
   rm "$t1"
 done
 rm "$t0"
